0aed6f645864d740b5fc6d7330656e186dd5c866
FreeTheMasons <mmullanax@gmail.com>
2018-01-25 19:52:15 -0800

    added age gate app

--- js/app.js
+++ js/app.js
@@ -1,25 +1,66 @@
-var firstName = prompt("what is your first name?");
+// var firstName = prompt("what is your first name?");
+//
+// var lastName = prompt("what is your last name?");
+//
+// console.log(firstName);
+//
+// alert(lastName);
+//
+// Var birthday = prompt("when is your birthday? mm/dd/yyyy");
+//
+// confirm(new Date(birthday));
+//
+//
+//
+//
+// var str1 = 'abc' ;
+// ‎var str2 = new String("abc") ;
+// ‎console.log(str1 === str2); //false
+// ‎console.log(str1 == str2); //true
+//
+// var num1 = prompt("enter a number", "10");
+// var num2 = prompt("enter a number", "10");
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+// alert(num1+num2);
+// var firstName = prompt("what is your first name?");
+//
+// var lastName = prompt("what is your last name?");
+//
+// console.log(firstName);
+//
+// alert(lastName);
+//
+// Var birthday = prompt("when is your birthday? mm/dd/yyyy");
+//
+// confirm(new Date(birthday));
+//
+//
+//
+//
+// var str1 = 'abc' ;
+// ‎var str2 = new String("abc") ;
+// ‎console.log(str1 === str2); //false
+// ‎console.log(str1 == str2); //true
+//
+// var num1 = prompt("enter a number", "10");
+// var num2 = prompt("enter a number", "10");
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+// alert(num1+num2);
 
-var lastName = prompt("what is your last name?");
+var age = prompt('what is your age?');
 
-console.log(firstName);
+// age = parseInt(age);
 
-alert(lastName);
-
-Var birthday = prompt("when is your birthday? mm/dd/yyyy");
-
-confirm(new Date(birthday));
-
-
-
-
-var str1 = 'abc' ;
-‎var str2 = new String("abc") ;
-‎console.log(str1 === str2); //false
-‎console.log(str1 == str2); //true
-
-var num1 = prompt("enter a number", "10");
-var num2 = prompt("enter a number", "10");
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-alert(num1+num2);
+if(age >= 18 && age < 80) {
+  var star = prompt('do you like star wars? yes or no');
+  if(star != 'no'){
+    alert('you are pretty cool')
+  }
+} else if(age < 18) {
+  alert('you are not allowed, you are too young');
+}
+  else if(age > 80){
+    prompt('do you like prunes?');
+  }
