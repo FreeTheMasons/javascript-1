72d1700b830f608fa99caa380f96271ae1a02b79
FreeTheMasons <mmullanax@gmail.com>
2018-01-26 20:45:52 -0800

    added things from day 4, committing so i can switch branches

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// var arr = new Array(1,2)
+// console.log(arr)
+//
+// var me = ['Game of Thrones', 19, true, 2015]
+//
+// console.log(me, me.length);
+// console.log(Array.isArray(me));
+//
+// var favoriteDog = prompt('who is your favorite favorite dog?');
+// var num = prompt('enter a number');
+// me.push(parseInt(num), favoriteDog);
+// console.log(me);
+//
+// me.splice(2, 1, 'mason', []); //starts at 3rd item, deletes 1 item at origin, adds string and array
+// console.log(me)
+
+var fav1 = ['BOTW', 'Link']
+var fav2 = [' Ocarina', 'Navi']
+var fav3 = [' What Remains of Edith Finch?', 'Edith']
+var fav4 = [' Journey', 'Robe dude']
+var fav5 = [' Minecraft', 'Pickaxe']
+var favGames = []
+favGames.push(fav1[0], fav2[0], fav3[0], fav4[0], fav5[0]);
+alert(favGames);
+var bestWorst = [fav1, fav5]
+console.log(bestWorst)
+alert(bestWorst.length)
+
+//to do: age gate program with date object and day 3 take home
